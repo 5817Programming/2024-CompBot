3dff4460c58485671267e89b0b0d4a2edf345749
Joshua phrachanhsay <jnphrachanhsay5817@gmai.com>
2024-02-06 16:21:14 -0800

    path changes

--- src/main/java/com/wcp/frc/Robot.java
+++ src/main/java/com/wcp/frc/Robot.java
@@ -141,11 +141,10 @@ HashMap<String,AutoBase> autos = new HashMap<String,AutoBase>();
     swerve.zeroModules();
 
     
-    // if(autoChooser.getSelected() != null){
-    //   autoChooser.getSelected().runAuto();
-    // }else{
-    // }
-    new M6().runAuto();
+    if(autoChooser.getSelected() != null){
+      autoChooser.getSelected().runAuto();
+    }else{
+    }
 
   }
 
@@ -155,7 +154,7 @@ HashMap<String,AutoBase> autos = new HashMap<String,AutoBase>();
 
   }
 
-  /** This function is called once when teleop is enabled. */
+  /** This function is called once when teleop is enabled. */  
   @Override
   public void teleopInit() {
     SuperStructure.getInstance().idleState();
