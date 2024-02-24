e450f03313125b7655b813f6401dc5724343369b
Mike <sangerroboticsprogramming@gmail.com>
2024-02-23 16:13:38 -0800

    m7 Coded

--- src/main/java/com/uni/frc/Autos/M7.java
+++ src/main/java/com/uni/frc/Autos/M7.java
@@ -6,7 +6,6 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.path.PathPlannerTrajectory;
 import com.uni.frc.subsystems.SuperStructure;
 import com.uni.frc.subsystems.Swerve.SwerveDrive;
-import com.uni.lib.geometry.Pose2d;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -24,30 +23,32 @@ public class M7 extends AutoBase{
         s.shootState();
 
         s.waitForTrajectoryState(0.7);
-        s.intakeState(false,);
+        s.intakeState(false,1.8);
+        s.waitForTrajectoryState(1.8);
         s.shootState();
 
         s.waitForTrajectoryState(2);
-        s.intakeState(false,);
+        s.intakeState(false,2.89);
+        s.waitForTrajectoryState(2.89);
         s.shootState();
 
         s.waitForTrajectoryState(3.2);
-        s.intakeState(false,);
+        s.intakeState(false,3.9);
         s.waitForTrajectoryState(3.9);
         s.shootState();
 
         s.waitForTrajectoryState(5.2);
-        s.intakeState(false,);
+        s.intakeState(false,7.35);
         s.waitForTrajectoryState(7.35);
         s.shootState();
 
         s.waitForTrajectoryState(8.13);
-        s.intakeState(false,);
+        s.intakeState(false,10.15);
         s.waitForTrajectoryState(10.15);
         s.shootState();
 
         s.waitForTrajectoryState(11.24);
-        s.intakeState(false,);
+        s.intakeState(false,12.94);
         s.waitForTrajectoryState(12.94);
         s.shootState();
 
